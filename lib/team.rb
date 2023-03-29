5ec67d9ad7aad9858749c542b67b752e91bfc80c
Michael Callahan <mcalla242@gmail.com>
2023-03-29 10:09:32 -0600

    Add solutions and testing specs for iteration 3

--- lib/team.rb
+++ lib/team.rb
@@ -1,5 +1,5 @@
 class Team
-  attr_reader :roster, :player_count
+  attr_reader :roster, :player_count, :long_term, :short_term, :value
   def initialize(name, location)
     @name = name
     @location = location
@@ -12,5 +12,25 @@ class Team
 
   def player_count
     @roster.count
+  end
+
+  def long_term_players
+    long_term = @roster.select {|player| player.contract_length > 24}
+  end
+
+  def short_term_players
+    short_term = @roster.select {|player| player.contract_length <= 24}
+  end
+
+  def total_value
+    value = @roster.map {|player| player.total_cost}
+    value.sum
+  end
+
+  def details
+    {
+      "total_value" => self.total_value,
+      "player_count" => self.player_count
+    }
   end
 end
